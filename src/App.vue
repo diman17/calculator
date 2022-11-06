82abf2ead91421180bb3b9ec871c588470e4f05c
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2022-11-06 17:38:36 +0300

    init: start project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="font-worksans text-3xl font-normal">Calculator</div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.vue"],
+  theme: {
+    extend: {
+      fontFamily: {
+        worksans: ["Work Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
